dfdd5d013f18ed84214c92eeebf29e0d9a365542
HMLani <23042538@myrp.edu.sg>
2025-01-20 12:45:38 +0800

    Add initial project structure with components, context, and styles

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
+
+const WorkoutForm = () => {
+    const {dispatch} = useWorkoutsContext();
+    const [title, setTitle] = useState('');
+    const [reps, setReps] = useState('');
+    const [load, setLoad] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSumit = async (e) => {
+        e.preventDefault();
+        const workout = { title, load, reps };
+
+        const res = await fetch('/api/workouts', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        const json = await res.json();
+        if (!res.ok) {
+            setError(json.error);
+        }
+
+        if (res.ok) {
+            setTitle('');
+            setLoad('');
+            setReps('');
+            setError(null);
+            console.log('Workout added', json);
+            dispatch({ type: 'CREATE_WORKOUT', payload: json})
+        }
+    };
+    
+    
+    return (
+        <form className='create' onSubmit={handleSumit}>
+            <h3>Add New Workout</h3>
+
+            <label>Workout Title:</label>
+            <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} />
+            
+            <label>Load (kg):</label>
+            <input type='number' onChange={(e) => setLoad(e.target.value)} value={load} />
+            
+            <label>Reps:</label>
+            <input type='number' onChange={(e) => setReps(e.target.value)} value={reps} />
+
+            <button>Add Workout</button>
+            {error && <div className='error'>{error}</div>}
+        </form>
+    );
+};
+
+export default WorkoutForm;
